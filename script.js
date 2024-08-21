bbdfce427bcc8ae9f57cb08dee4b70e3c6879c29
giftois <giftywanzola@outlook.com>
2024-08-22 00:42:16 +0100

    Files

---
+++ script.js
@@ -0,0 +1,18 @@
+/*!Tasks:
+Create a new index.html and script.js in order to complete the following tasks:
+
+1. Add two buttons with IDs showButton and hideButton. 
+Add an image with the ID myImage and set its display style 
+to none initially so it's hidden when the page loads.
+ 
+2. Use CSS to set the initial display property of 
+the image to none.
+ 
+3. Get references to the buttons and the image using 
+getElementById.
+
+4. Add an event listener to the "Show" button that 
+sets the display property of the image to block when clicked.
+
+5 Add an event listener to the "Hide" button that sets 
+the display property of the image to none when clicked.*/
